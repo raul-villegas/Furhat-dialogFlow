e5a5460e87b51841d9e97470f49510dc48cde076
raul-villegas <127860226+raul-villegas@users.noreply.github.com>
2023-04-14 11:54:03 +0200

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,39 +1,46 @@
-import pandas as pd
+import csv
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Read the conversation data from the CSV file
-df = pd.read_csv('results.csv')
+filename = "results.csv"  # replace with the actual filename
 
-# Get the unique conversation IDs
-conversations = df['conversation_id'].unique()
+# Create empty lists to store data
+conversations = []
+conversation_lengths = []
+fallback_rates = []
+color_google = ['#f39c12', '#e74c3c', '#16a085', '#3498db', '#9b59b6']
+color_default= ["#ff9999", "#66b3ff", "#99ff99", "#ffcc99", "#d3d3d3"]
 
-# Calculate the fallback rates for each conversation
-fallback_rates = {conversation: df.loc[df['conversation_id'] == conversation, 'fallback_rate'].mean() for conversation in conversations}
+# Open the CSV file and create a reader object
+with open(filename, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
 
-# Calculate the conversation lengths for each conversation
-conversation_lengths = df.groupby('conversation_id')['conversation_length'].sum()
+    # Iterate over each row in the CSV file
+    for row in reader:
+        conversation_id = row["conversation_id"]
+        conversation_length = int(row["conversation_length"])
+        fallback_rate = float(row["fallback_rate"])
 
-# Set up color scheme
-color_palette = ['#f39c12', '#e74c3c', '#16a085', '#3498db', '#9b59b6']
-color_map = {conversation: color_palette[i % len(color_palette)] for i, conversation in enumerate(conversations)}
-colors = [color_palette[i % len(color_palette)] for i in range(len(conversations))]
+        # Add the conversation ID and length to the lists
+        conversations.append(conversation_id)
+        conversation_lengths.append(conversation_length)
+        fallback_rates.append(fallback_rate)
 
-# Create bar chart
+# Set up the bar chart
 fig, ax = plt.subplots()
-x = np.arange(len(conversations))
-ax.bar(x, conversation_lengths.values, color=[color_map[conversation] for conversation in conversations])
-ax.set_xticks(x)
-ax.set_xticklabels(conversations, rotation=45, ha='right')
-ax.set_ylabel('Conversation Length')
-ax.set_title('Conversation Lengths')
-plt.tight_layout()
+ax.set_title("Conversation Lengths by Conversation")
+ax.set_xlabel("Conversation ID")
+ax.set_ylabel("Length")
+ax.bar(conversations, conversation_lengths, color= color_google)
 
-# Create pie chart
+# # Show the bar chart
+# plt.show()
+
+# Set up the pie chart
 fig, ax = plt.subplots()
-ax.pie(fallback_rates.values(), labels=conversations, colors=colors, autopct='%1.1f%%')
-ax.set_title('Fallback Rates')
+ax.set_title("Fallback Rates by Conversation")
+ax.pie(fallback_rates, labels=conversations, colors=color_google, autopct='%1.1f%%')
 
-# Show the chart
+# Show both charts
 plt.show()
+
 
